dc84c5e7ba4624240b9a5594d5c8fd6e9fb73b6b
Matheus Morillo <matheus.morillo2000@gmail.com>
2023-08-12 20:26:03 -0300

    Initial commit

---
+++ src/main/java/com/example/ecommercemessaging/EcommerceMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMessagingApplication.class, args);
+	}
+
+}
